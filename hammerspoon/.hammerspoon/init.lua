4920bbbc42f6b8381893642e10c33469659ffdaa
darrylanderson <6097929+darrylanderson@users.noreply.github.com>
2019-05-24 14:13:06 -0500

    Switched to a tiling window management setup with undo

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,8 +1,4 @@
 
--- Spectacle-like window movement and resizing
-hs.loadSpoon("Lunette")
-spoon.Lunette:bindHotkeys()
-
 -- Hyper key bindings
 local hyper = { "cmd", "alt", "ctrl", "shift" }
 
@@ -10,46 +6,27 @@ local hyper = { "cmd", "alt", "ctrl", "shift" }
 hs.hotkey.bind(hyper, "0", function()
   hs.reload()
 end)
-
 hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+
+-- Lock screen
+hs.hotkey.bind(hyper, 'l', function()
+  hs.caffeinate.lockScreen()
+end)
 
 -- App launchers
 local applicationKeys = {
   c = 'Google Chrome',
   t = 'iTerm',
-  s = 'Slack'
+  s = 'Slack',
+  f = 'Finder'
 }
-
 for key,app in pairs(applicationKeys) do
   hs.hotkey.bind(hyper, key, function()
     hs.application.launchOrFocus(app)
   end)
 end
 
--- Alternative window management
+-- Window management
 hs.window.animationDuration = 0
-
-hs.hotkey.bind(hyper, "h", function()
-  local win = hs.window.focusedWindow();
-  if not win then return end
-win:moveToUnit(hs.layout.left50)
-end)
-
-hs.hotkey.bind(hyper, "j", function()
-  local win = hs.window.focusedWindow();
-  if not win then return end
-win:moveToUnit(hs.layout.maximized)
-end)
-
-hs.hotkey.bind(hyper, "k", function()
-  local win = hs.window.focusedWindow();
-  if not win then return end
-win:moveToScreen(win:screen():next())
-end)
-
-hs.hotkey.bind(hyper, "l", function()
-  local win = hs.window.focusedWindow();
-  if not win then return end
-win:moveToUnit(hs.layout.right50)
-end)
+require("tiling")
 
